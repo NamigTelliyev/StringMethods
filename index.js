3ac74280c2eebdc1baec79fdb4f656d779f62ee5
NamigTelliyev <146194992+NamigTelliyev@users.noreply.github.com>
2023-10-22 22:39:19 +0400

    string methods

---
+++ index.js
@@ -0,0 +1,84 @@
+//! Sual 1 - hell edə bilmədim :(
+
+//! Sual 2
+// let a =23;
+
+// for (let i = 1; i <= 1000; i++) {
+//     if (i % a === 0) {
+//         console.log(i);
+//     }
+// }
+
+//! ARRAY 3-7ci suallar
+// const arr = [2, 4, 5, 9, 1];
+
+//! Sual 3
+// let minimum = arr[0];
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < minimum) {
+//         minimum = arr[i];
+//     }
+// }
+// console.log(minimum);
+
+//! Sual 4
+// let maximum = arr[0];
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > maximum) {
+//         maximum = arr[i];
+//     }
+// }
+// console.log(maximum);
+
+//! Sual 5
+// let minimum = arr[0];
+// let minimumIndex = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < minimum) {
+// minimumIndex = i;
+//     }
+// }
+// console.log(minimumIndex);
+
+
+//! Sual 6
+// let maximum = arr[0];
+// let maximumIndex = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > maximum) {
+// maximumIndex = i;
+//     }
+// }
+// console.log(maximumIndex);
+
+//! Sual 7
+// let indexPlus = 0;
+
+// for (let i = 1; i < arr.length; i += 2) {
+//     indexPlus += arr[i];
+// }
+// console.log(indexPlus);
+
+//! Sual 8
+// let word = "Mən Code Academy Tələbəsiyəm"
+// console.log(word.toLowerCase());
+
+//! Sual 9
+// let word = "            Mən Code Academydə Programing tədrisi alıram            "
+// console.log(word.trim());
+
+
+//! Sual 10
+// let word = "Mən Code Academy Tələbəsiyəm"
+// console.log(word.slice(0,3));
+// console.log(word.slice(4,8));
+// console.log(word.slice(9,16));
+// console.log(word.slice(17,28));
+
+//! Sual 11
+// let word = "            Mən Code Academydə Programing tədrisi alıram            "
+// console.log(word.length);
